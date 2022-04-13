2761e4c4306629d10332a92e4a31ce6b84f73601
yyz <365939114@qq.com>
2022-04-13 19:07:10 +0800

    update

--- haokanzhan/main.py
+++
@@ -1,6 +0,0 @@
-'''
-start spider
-'''
-from scrapy.cmdline import execute
-
-execute(['scrapy', 'crawl', 'movie'])
--- yingcai/__init__.py
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/python
-# -*- coding: utf-8 -*-
-"""
-# @Author : Yang YeZhuang
-# @Time : 2020/10/10 15:58
-# @File : __init__.py.py
-"""
